65a3eeddea910ecd1384d7c4b1081bddaf63004c
Xiaolin-Jiang <90145823+Xiaolin-Jiang@users.noreply.github.com>
2021-09-06 21:45:27 +0200

    add dataset document

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Prices of 50,000 round cut diamonds.
+#'
+#' A dataset create the first graph at the wikipedia page.
+#'
+#' @format A data frame with 18 rows and 3 variables:
+#' \describe{
+#'   \item{v1, v2}{contains the edges of the graph (from v1 to v2)}
+#'   \item{w}{contains the weight of the edge (w)}
+#'   ...
+#' }
+#' @source \url{(https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm}
+"wiki_graph"
